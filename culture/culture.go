4a503ef39b8c9b52fbcac044219d136a245592bb
olehmushka <olegamysk@gmail.com>
2022-11-06 22:56:05 +0200

    added cultures

--- culture/culture.go
+++ culture/culture.go
@@ -1,6 +1,17 @@
 package culture
 
 import (
+	"encoding/json"
+	"fmt"
+	"io/fs"
+	"io/ioutil"
+	"path"
+	"runtime"
+	"sync"
+
+	"github.com/olehmushka/golang-toolkit/either"
+	sliceTools "github.com/olehmushka/golang-toolkit/slice_tools"
+	"github.com/olehmushka/golang-toolkit/wrapped_error"
 	genderAcceptance "github.com/olehmushka/world-generator-engine/gender_acceptance"
 	genderDominance "github.com/olehmushka/world-generator-engine/gender_dominance"
 )
@@ -27,4 +38,50 @@ type Culture struct {
 	LanguageSlug    string                      `json:"language_slug" bson:"language_slug"`
 	GenderDominance genderDominance.Dominance   `json:"gender_dominance" bson:"gender_dominance"`
 	MartialCustom   genderAcceptance.Acceptance `json:"martial_custom" bson:"martial_custom"`
+}
+
+func LoadAllRawCultures() chan either.Either[[]*RawCulture] {
+	_, filename, _, _ := runtime.Caller(1)
+	currDirname := path.Dir(filename) + "/"
+	dirname := currDirname + "/data/cultures/"
+	ch := make(chan either.Either[[]*RawCulture], MaxLoadDataConcurrency)
+	go func() {
+		files, err := ioutil.ReadDir(dirname)
+		if err != nil {
+			ch <- either.Either[[]*RawCulture]{Err: wrapped_error.NewInternalServerError(err, fmt.Sprintf("can not read dir by dirname (dirname=%s)", currDirname))}
+			return
+		}
+
+		var wg sync.WaitGroup
+		wg.Add(len(files))
+		for _, file := range files {
+			go func(file fs.FileInfo) {
+				defer wg.Done()
+				if file.IsDir() {
+					return
+				}
+				filename := dirname + file.Name()
+				b, err := ioutil.ReadFile(filename)
+				if err != nil {
+					ch <- either.Either[[]*RawCulture]{Err: err}
+					return
+				}
+				var sfs []*RawCulture
+				if err := json.Unmarshal(b, &sfs); err != nil {
+					ch <- either.Either[[]*RawCulture]{Err: err}
+					return
+				}
+				if len(sfs) == 0 {
+					return
+				}
+				for _, chunk := range sliceTools.Chunk(MaxLoadDataChunkSize, sfs) {
+					ch <- either.Either[[]*RawCulture]{Value: chunk}
+				}
+			}(file)
+		}
+		wg.Wait()
+		close(ch)
+	}()
+
+	return ch
 }
--- culture/culture_test.go
+++ culture/culture_test.go
@@ -1,1 +1,18 @@
 package culture
+
+import "testing"
+
+func TestLoadAllRawCultures(t *testing.T) {
+	var count int
+	for chunk := range LoadAllRawCultures() {
+		if chunk.Err != nil {
+			t.Fatalf("unexpected error (err=%+v)", chunk.Err)
+			return
+		}
+		if len(chunk.Value) == 0 {
+			t.Fatalf("unexpected length of raw_cultures")
+		}
+		count += len(chunk.Value)
+	}
+	t.Logf("count:%d\n", count)
+}
